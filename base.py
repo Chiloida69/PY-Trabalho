a7f721cb2425810253d2e336b22adb2ba5d8e9a9
Thiago <thiagovieira.unv@gmail.com>
2025-11-26 19:43:28 -0300

    subindo tudo

--- base.py
+++ base.py
@@ -14,6 +14,8 @@ load_dotenv()
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 
+#HELLO
+
 # Cria o cliente do Supabase  
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY) 
 
